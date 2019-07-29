944cef959a2195e3af9714918bc6d92292b311ec
Andrew Barrette <barrette.andrew.dev@gmail.com>
2019-07-28 23:01:26 -0500

    add a form submission test

--- cypress/integration/solutiondesign.spec.js
+++ cypress/integration/solutiondesign.spec.js
@@ -16,4 +16,48 @@ describe('solutiondesign.com', () => {
 				expect(pathname).to.eq('/web/guest/we-re-hiring')
 			})
 	})
+
+	it('successfully submits a FED application', () => {
+		const NAME = 'Andrew Barrette'
+		const EMAIL = 'drew.barrette@solutiondesign.com'
+		const URL = '/web/guest/senior-front-end-developer'
+
+		// Set up routes to watch
+		cy.server()
+			.route('POST', URL + '**')
+			.as('postResume')
+
+		// Fill basic form fields
+		cy.visit(URL)
+			.get('input[type=text]:eq(0)')
+			.scrollIntoView()
+			.type(NAME)
+			.get('input[type=text]:eq(1)')
+			.type(EMAIL)
+
+		// File upload helper function
+		const upload = (selector, name, type) =>
+			cy
+				.fixture(name, 'base64')
+				.then(Cypress.Blob.base64StringToBlob)
+				.then(blob => {
+					const file = new File([blob], name, { type })
+					const dataTransfer = new DataTransfer()
+					dataTransfer.items.add(file)
+
+					return cy.get(selector).then($el => {
+						const rawDomElement = $el[0]
+						rawDomElement.files = dataTransfer.files
+					})
+				})
+
+		// Upload the file and click submit
+		upload('input[type=file]', 'resume.txt', 'text/plain')
+			.get('button[type=submit]')
+			.click()
+			.wait('@postResume')
+
+		// Assert that we show a "thank you" message
+		cy.contains('thank you for applying')
+	})
 })
